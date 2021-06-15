03327fd9b5c23891973ef33b069ac4812637a87e
Exlley Santos <exlleyclemente@gmail.com>
2021-06-15 11:59:05 -0300

    Edit user controller

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -6,7 +6,7 @@ class UserController {
 	async index() {
 		return User.all();
 	}
-	async create({ request }) {
+	async store({ request }) {
 		const data = request.only(['email', 'password']);
 
 		const user = await User.create(data);
@@ -18,7 +18,7 @@ class UserController {
 		const user = await User.find(params.id);
 
 		if (!user) {
-			return response.status(404).send({ error: 'Not found' });
+			return response.status(404).send({ error: 'User not found' });
 		}
 
 		return user;
